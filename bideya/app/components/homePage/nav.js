daf61a21295148f66bff0b241c3fef49bb644c13
arij cheikhrouhou <aroujaa26@gmail.com>
2025-03-07 13:06:19 +0000

    nav fixe

--- bideya/app/components/homePage/nav.js
+++ bideya/app/components/homePage/nav.js
@@ -1,12 +1,30 @@
 // components/NavBar.js
+
+"use client";
+import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import { FiSearch } from 'react-icons/fi';
 
 const NavBar = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
-    <nav className="flex items-center justify-between px-3 py-2 bg-white shadow-sm">
+    <nav className={`flex items-center justify-between px-3 py-2 bg-white  ${isScrolled ? 'fixed top-0 left-0 right-0 z-40' : ''}`}>
       {/* Logo centré */}
-      <div className="text-4xl font-extrabold text-blue-900 ml-8"> {/* Taille et position de "Bideya Boost" */}
+      <div className="text-2xl font-extrabold text-blue-900 ml-8"> {/* Taille et position de "Bideya Boost" */}
         Bideya Boost
       </div>
 
@@ -18,7 +36,7 @@ const NavBar = () => {
           </Link>
         </li>
         <li>
-          <Link href="/categories" className="text-blue-900 hover:text-blue-700">
+          <Link href="categorie" className="text-blue-900 hover:text-blue-700">
             Categories
           </Link>
         </li>
@@ -33,7 +51,7 @@ const NavBar = () => {
           </Link>
         </li>
         <li>
-          <Link href="./infoSession.js" className="text-blue-900 hover:text-blue-700">
+          <Link href="/features" className="text-blue-900 hover:text-blue-700">
             Features
           </Link>
         </li>
--- bideya/app/page.js
+++ bideya/app/page.js
@@ -4,7 +4,7 @@ import Footer from "./components/homePage/footer"
 import Hero from "./components/homePage/home"
 import About from "./components/homePage/aboutUs";
 import Categorie  from "./components/homePage/categorie";
-import Info from "./components/homePage/infoSession"
+
 import BOOKING from "./components/boocking/boock";
 import Confirm from "./components/boocking/confirm"
 import Hackathon  from "./components/hackaton/hackaton";
@@ -20,7 +20,7 @@ export default function Home() {
     <Hero />
     <About />
     <Categorie/>
-  <Info/>
+  
     <Footer/>
     <BOOKING/>
     <Confirm/>
